996c7eea943352f4a2d9512af0995453a6d6afda
Lele1409 <leander.decoene@gmail.com>
2026-05-18 18:51:08 +0200

    update: recorder.cpp do TODO from Wiemann

--- w2/B/2.2/trajectory_recorder/src/recorder.cpp
+++ w2/B/2.2/trajectory_recorder/src/recorder.cpp
@@ -11,26 +11,38 @@ using std::placeholders::_1;
 class TrajectoryRecorder : public rclcpp::Node
 {
 public:
-    TrajectoryRecorder(std::ofstream& odom_out, std::ofstream& imu_out) : 
+    TrajectoryRecorder(std::ofstream& odom_out, std::ofstream& imu_out) :
         Node("trajectory_recorder"), odom_out_(odom_out), imu_out_(imu_out)
     {
- 
-        // ToDo: Initialize required subscribers 
-        // for odometry and imu topics. Register 
-        // corresponding callbacks
+        odom_sub_ = create_subscription<nav_msgs::msg::Odometry>(
+            "/odom", 10,
+            std::bind(&TrajectoryRecorder::odom_callback, this, _1));
 
+        imu_sub_ = create_subscription<sensor_msgs::msg::Imu>(
+            "/imu", 10,
+            std::bind(&TrajectoryRecorder::imu_callback, this, _1));
     }
 
 
 private:
 
-    // You can use this streams to output
-    // the coordinates of your computed
-    // poses
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        odom_out_ << msg->pose.pose.position.x << " "
+                  << msg->pose.pose.position.y << "\n";
+    }
+
+    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+    {
+        imu_out_ << msg->linear_acceleration.x << " "
+                 << msg->linear_acceleration.y << " "
+                 << msg->angular_velocity.z << "\n";
+    }
+
     std::ofstream& odom_out_;
     std::ofstream& imu_out_;
-
-
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
 };
 
 int main(int argc, char** argv)
